47ec9b71b3388a544f29facda248daab8141d68e
Thomas Rubas <kumarthomas26@gmail.com>
2024-10-23 19:18:28 +0530

    find value in single dimensional array

--- src/arrays/ArrayFunctions.java
+++ src/arrays/ArrayFunctions.java
@@ -1,14 +1,14 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class ArrayFunctions {
     int[] array;
 
     public ArrayFunctions(int sizeOfArray) {
         array = new int[sizeOfArray];
         //Filling the array with default value for array manipulations
-        for (int i = 0; i < array.length; i++) {
-            array[i] = Integer.MIN_VALUE;
-        }
+        Arrays.fill(array, Integer.MIN_VALUE);
     }
 
     //Method to insert values into array
@@ -25,11 +25,22 @@ public class ArrayFunctions {
         }
     }
 
+    public void findValue(int value) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == value) {
+                System.out.println("Value " + value + " found at index [" + i + "]");
+                return;
+            }
+        }
+        System.out.println("The value " + value + " not found");
+    }
+
     public static void main(String[] args) {
         ArrayFunctions af = new ArrayFunctions(3);
         af.insert(0, 1);
         af.insert(1, 2);
         af.insert(1, 1);
         af.insert(5, 1);
+        af.findValue(1);
     }
 }
